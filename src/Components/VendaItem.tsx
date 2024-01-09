2a26e84cd9ae51a10c09a0e0e80f280270b1cae5
DanielChrisostomo <daniluz1515@gmail.com>
2024-01-09 15:35:00 -0300

    Venda Item

---
+++ src/Components/VendaItem.tsx
@@ -0,0 +1,22 @@
+import { IVenda } from "../Context/DataContext";
+
+const VendaItem = ({ venda }: { venda: IVenda }) => {
+  console.log(venda);
+
+  return (
+    <div className="venda box">
+      <a href="" style={{ fontFamily: "monospace" }}>
+        {venda.id}
+      </a>
+      <div>{venda.nome}</div>
+      <div>
+        {venda.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default VendaItem;
